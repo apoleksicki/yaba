e049c3ede62cd6d4e8c60ebdc0c3c9c2d259ecd3
Antoni Oleksicki <ap.oleksicki@gmail.com>
2023-01-11 20:09:59 +0100

    Add test for spendings view

--- spendings/tests.py
+++ spendings/tests.py
@@ -1,3 +1,12 @@
+import os
 from django.test import TestCase
 
-# Create your tests here.
+from spendings import models
+
+
+class TestSpendingsView(TestCase):
+    def test_spendings_index(self) -> None:
+        response = self.client.get("/spendings/")
+        self.assertEqual(
+            "Hello, world. You're at the spendings index.", response.content.decode()
+        )
