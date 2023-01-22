4b589401367805698a896c70a24915c3a8ff394a
Antoni Oleksicki <ap.oleksicki@gmail.com>
2023-01-22 22:35:17 +0100

    Add basic spendings view

--- spendings/tests.py
+++ spendings/tests.py
@@ -1,3 +1,5 @@
+from datetime import date
+from decimal import Decimal
 import os
 from django.test import TestCase
 
@@ -5,8 +7,31 @@ from spendings import models
 
 
 class TestSpendingsView(TestCase):
+    def setUp(self) -> None:
+        self.category = models.Category.objects.create(name="fun")
+        self.spending_1 = models.Spending.objects.create(
+            date_spent=date.fromisoformat("2023-01-11"),
+            amount_spent=Decimal(11.5),
+            description="cinema",
+            category=self.category,
+        )
+        self.spending_2 = models.Spending.objects.create(
+            date_spent=date.fromisoformat("2023-01-10"),
+            amount_spent=Decimal(1.5),
+            description="newspaper",
+            category=self.category,
+        )
+
     def test_spendings_index(self) -> None:
         response = self.client.get("/spendings/")
-        self.assertEqual(
-            "Hello, world. You're at the spendings index.", response.content.decode()
+        content = response.content.decode()
+        spendings = sorted(
+            [self.spending_1, self.spending_2], key=lambda s: s.date_spent
         )
+        expected_content = "\n".join(
+            [
+                f"{s.date_spent}, {s.description}, {'{0:.2}'.format(s.amount_spent)}, {s.category.name}"
+                for s in spendings
+            ]
+        )
+        self.assertIn(expected_content, content)
--- spendings/views.py
+++ spendings/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
 
+from . import models
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the spendings index.")
+    spendings = models.Spending.objects.order_by("date_spent")
+    response_content = "\n".join(
+        [
+            f"{s.date_spent}, {s.description}, {s.amount_spent}, {s.category.name}"
+            for s in spendings
+        ]
+    )
+    return HttpResponse(response_content)
