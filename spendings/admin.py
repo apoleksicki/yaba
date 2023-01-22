05db5d8fedccf81553e754741c20044140b3eb12
Antoni Oleksicki <ap.oleksicki@gmail.com>
2023-01-22 22:42:00 +0100

    Add account

--- spendings/admin.py
+++ spendings/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
+from .models import Account
 from .models import Category
 from .models import Spending
 
+admin.site.register(Account)
 admin.site.register(Category)
 admin.site.register(Spending)
--- spendings/models.py
+++ spendings/models.py
@@ -5,8 +5,13 @@ class Category(models.Model):
     name = models.CharField(max_length=200)
 
 
+class Account(models.Model):
+    name = models.CharField(max_length=200)
+
+
 class Spending(models.Model):
     date_spent = models.DateField("date spent")
     amount_spent = models.DecimalField(max_digits=8, decimal_places=2)
     description = models.CharField(max_length=200)
     category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
