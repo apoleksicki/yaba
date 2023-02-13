1ad8ed922b71930b5c6d841662af02dcd8adf036
Antoni Oleksicki <ap.oleksicki@gmail.com>
2023-02-13 20:04:09 +0100

    Add template for spendings

--- spendings/views.py
+++ spendings/views.py
@@ -1,14 +1,11 @@
 from django.http import HttpResponse
+from django.template import loader
 
 from . import models
 
 
 def index(request):
     spendings = models.Spending.objects.order_by("date_spent")
-    response_content = "\n".join(
-        [
-            f"{s.date_spent}, {s.description}, {s.amount_spent}, {s.category.name}"
-            for s in spendings
-        ]
-    )
-    return HttpResponse(response_content)
+    template = loader.get_template('spendings/index.html')
+    context = {"spending_list": spendings}
+    return HttpResponse(template.render(context, request))
