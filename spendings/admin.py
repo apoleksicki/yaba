640cc82d1885d067b90d0a598a108081c5530447
Antoni Oleksicki <ap.oleksicki@gmail.com>
2024-02-21 13:06:38 +0100

    Add admin presentation objects for models

--- spendings/admin.py
+++ spendings/admin.py
@@ -1,11 +1,33 @@
 from django.contrib import admin
 
-from .models import Account
+from .models import Account, Estimate
 from .models import Category
 from .models import Transaction
 from .models import Budget
 
-admin.site.register(Account)
-admin.site.register(Category)
+
+class TransactionInline(admin.TabularInline):
+    model = Transaction
+    extra = 1
+
+
+class AccountAdmin(admin.ModelAdmin):
+    inlines = [TransactionInline]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [TransactionInline]
+
+
+class EstimateInline(admin.TabularInline):
+    model = Estimate
+
+
+class BudgetAdmin(admin.ModelAdmin):
+    inlines = [EstimateInline]
+
+
+admin.site.register(Account, AccountAdmin)
+admin.site.register(Category, AccountAdmin)
 admin.site.register(Transaction)
-admin.site.register(Budget)
+admin.site.register(Budget, BudgetAdmin)
