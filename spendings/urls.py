f60eee0421b2862ec72d7f6767715294ef1b8cfb
Antoni Oleksicki <ap.oleksicki@gmail.com>
2024-02-29 11:12:32 +0100

    Fix style

--- spendings/urls.py
+++ spendings/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("budgets/", views.budgets, name="budgets")
+    path("budgets/", views.budgets, name="budgets"),
 ]
--- spendings/views.py
+++ spendings/views.py
@@ -6,7 +6,6 @@ from django.shortcuts import render
 from . import models
 
 
-
 def index(request):
     spendings = models.Transaction.objects.order_by("date_spent")
     context = {"spending_list": spendings}
